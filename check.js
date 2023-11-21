892fc39e5eb3c6c0bb2251984fda441d89efd932
PalomaRmMz <p.ramirez.m.8080@gmail.com>
2023-11-21 14:08:19 -0600

    formulario validado

--- check.js
+++ check.js
@@ -1,18 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const checkboxes = document.querySelectorAll(
+    'input[name="grupos_prioritarios[]"]'
+  );
   const form = document.getElementById("myForm");
   const submitButton = document.getElementById("btnEnviar");
 
   function validateForm() {
     const isValid = form.checkValidity();
     submitButton.disabled = !isValid;
-    if (isValid) {
-      console.log("VALIDO");
-    } else {
-      console.log("INVALIDO");
-    }
   }
 
-  validateForm();
+  function handleCheckboxChange() {
+    const checkedCheckboxes = Array.from(checkboxes).some(
+      (checkbox) => checkbox.checked
+    );
+    checkboxes.forEach((checkbox) => {
+      checkbox.required = !checkedCheckboxes;
+    });
+    validateForm();
+  }
 
-  form.addEventListener("input", validateForm);
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", handleCheckboxChange);
+  });
+
+  // Verificar si al menos uno está seleccionado al cargar la página
+  handleCheckboxChange();
 });
